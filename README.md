67979e4fec00560d18f7ba92b67bb7e42932f35b
Brian <brian.pfeil@gmail.com>
2018-10-17 21:19:10 -0400

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# node-jenkins-api-playground
+
+project to learn [node-jenkins](https://github.com/silas/node-jenkins)
+
+## Install
+
+* `npm install jenkins`
+* copy `.env.sample` to `.env` and update with *"your"* values
+
+## Run Examples
+
+`node index.js`
+
+> shows getting details of `job` and `building a job`
---
+++ index.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+
+const util = require("util");
+
+const run_jenkins_examples = async () => {
+  const jenkins = require("jenkins")({
+    baseUrl: `https://${process.env.USERNAME}:${process.env.PASSWORD}@${
+      process.env.JENKINS_BASE_URL
+    }`,
+    crumbIssuer: true
+  });
+  const jobName = "PERSONAL/PFEIL/pipeline-test-02";
+
+  const jobConfig = () => {
+    jenkins.job.config(jobName, function(err, data) {
+      if (err) {
+        return console.error(err);
+      }
+      console.log("queue item number", data);
+    });
+  };
+
+  const build = () => {
+    // jenkins.job.build({ name: jobName, parameters: {} }, function(err, data) {
+    jenkins.job.build(jobName, function(err, data) {
+      if (err) {
+        return console.error(err);
+      }
+      console.log("queue item number", data);
+    });
+  };
+
+  jobConfig()
+  build()
+};
+
+run_jenkins_examples();
